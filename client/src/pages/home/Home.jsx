4ba9ed688bd8e30f35728e8f90a2881c6731c657
Hridoy Banik <hridoy.banik71@gmail.com>
2022-05-25 19:15:58 +0600

    add navbar components

---
+++ client/src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '../../components/navbar/Navbar';
+import './home.css'
+function Home(props) {
+    return (
+        <div>
+            <Navbar/>
+        </div>
+    );
+}
+
+export default Home;
